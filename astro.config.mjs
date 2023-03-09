1f2b72bc42cabd55e3e66bea8c4c5c4433bc1942
Mesbah Mowlavi <powerofm@users.noreply.github.com>
2023-03-09 08:01:18 -0800

    initial work

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  experimental: {
+    assets: true,
+  },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "bulma/sass/utilities/_all.sass";`,
+        },
+      },
+    },
+  },
+});
